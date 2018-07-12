b440714f1c5bc2b11166ad2f7b15c5ac28cae57f
soheemon <2170210@gmail.com>
2018-07-13 00:59:36 +0900

    encryption & decryption module test success

--- des/des.c
+++ des/des.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+
 typedef unsigned long long bit_64;
 typedef unsigned int bit_32;
 typedef struct _splited_56_28 {
@@ -23,6 +25,97 @@ typedef struct _bit_48 {
 
 bit_48 subkey[16];
 
+typedef struct _parity_chk {
+	unsigned char bit1:1,
+				  bit2:1,
+				  bit3:1,
+				  bit4:1,
+				  bit5:1,
+				  bit6:1,
+				  bit7:1,
+				  parity_bit:1;
+} parity_chk;
+
+bit_64 str_to_int_64(char* input) {
+	bit_64 output = 0;
+
+	while(*input) {
+		output <<=8;
+		output |= (bit_64)*input;
+		printf("1. %c\n", *input);
+		input++;
+	}
+	output |= '\n';
+	return output;
+}
+
+void int_to_str_64(bit_64* input) {
+	
+	char* output = (char*)input;
+
+	printf("%c\n", output[7]);
+	printf("%c\n", output[6]);
+	printf("%c\n", output[5]);
+	printf("%c\n", output[4]);
+	printf("%c\n", output[3]);
+	printf("%c\n", output[2]);
+	printf("%c\n", output[1]);
+	printf("%c\n", output[0]);
+}
+
+unsigned char parity_chk_8(unsigned char input) {
+
+	parity_chk parity;
+	unsigned char tmp = input;
+	tmp <<=1;
+
+	parity.bit1 = (tmp & 0x80) ? 1 : 0;
+	parity.bit2 = (tmp & 0x40) ? 1 : 0;
+	parity.bit3 = (tmp & 0x20) ? 1 : 0;
+	parity.bit4 = (tmp & 0x10) ? 1 : 0;
+	parity.bit5 = (tmp & 0x08) ? 1 : 0;
+	parity.bit6 = (tmp & 0x04) ? 1 : 0;
+	parity.bit7 = (tmp & 0x02) ? 1 : 0;
+
+	parity.parity_bit = ((parity.bit1 ^ parity.bit2) ^ (parity.bit3 ^ parity.bit4)) ^ ((parity.bit5 ^ parity.bit6) ^ parity.bit7) ? 1 : 0;
+	unsigned char output = 0;
+
+	output |= parity.bit1 ? 0x80 : 0;
+	output |= parity.bit2 ? 0x40 : 0;
+	output |= parity.bit3 ? 0x20 : 0;
+	output |= parity.bit4 ? 0x10 : 0;
+	output |= parity.bit5 ? 0x08 : 0;
+	output |= parity.bit6 ? 0x04 : 0;
+	output |= parity.bit7 ? 0x02 : 0;
+	output |= parity.parity_bit ? 0x01 : 0;
+
+	return output;
+}
+
+bit_64 parity_chk_64(bit_56 input) {
+	bit_56 tmp_56;
+	tmp_56.bit56 = 0;
+	unsigned char tmp_8[8] = {0}; 
+
+	for(int i = 0; i < 8; i++) {
+		tmp_56.bit56 = input.bit56 & ((unsigned long long )0x7F <<(7 * i));
+		tmp_8[i] = (tmp_56.bit56 >> (7 * i));
+	}
+
+	unsigned char parity_ret[8] = {0};
+	for(int i = 0; i < 8; i++) {
+		parity_ret[i] = parity_chk_8(tmp_8[i]);
+	}
+	bit_64 output = 0;
+	for(int i = 7; i >= 0; i--) {
+
+		output <<= 8;
+		output |= (bit_64)parity_ret[i];
+	}
+
+//	printf("%llu\n", (unsigned long long)output);
+	return output;
+}
 splited_56_28 permutation_c_1(bit_64 input) {
 	
 	splited_56_28 output;
@@ -32,12 +125,12 @@ splited_56_28 permutation_c_1(bit_64 input) {
 	output.left = 0;
 	output.right = 0;
 
-	char left_table[28] = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36 };
+	char left_table[28] = {57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36 };
 	char right_table[28] = {63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4};
 
 	for(int i = 0; i < 28; i++) {
 
-		temp = input & (0x8000000000000000 >> (left_table[i] - 1)); 
+		temp = input & ((bit_64)0x8000000000000000 >> (left_table[i] - 1));
 		temp = temp << (left_table[i] - 1);
 		temp = temp >> i;
 
@@ -50,7 +143,7 @@ splited_56_28 permutation_c_1(bit_64 input) {
 
 	for(int i = 0; i < 28; i++) {
 
-		temp = input & (0x8000000000000000 >> (right_table[i] - 1)); 
+		temp = input & ((bit_64)0x8000000000000000 >> (right_table[i] - 1)); 
 		temp = temp << (right_table[i] - 1);
 		temp = temp >> i;
 
@@ -64,8 +157,8 @@ splited_56_28 permutation_c_1(bit_64 input) {
 
 void left_shift_n(splited_56_28* input, int n) {
 
-	input->left = (input->left << n) | (input->left >> 28-1);
-	input->right = (input->right << n) | (input->right >> 28-1);
+	input->left = (input->left << n) | (input->left >> 28-n);
+	input->right = (input->right << n) | (input->right >> 28-n);
 
 }
 
@@ -100,7 +193,7 @@ splited_64_32 seperate_64(bit_64 input) {
 	output.right = 0;
 
 	output.left = input >> 32;
-	output.right = input << 32;
+	output.right = (uint32_t)input;
 
 	return output;
 }
@@ -114,6 +207,7 @@ bit_48 permutation_c_2(bit_56 input) {
 	bit_48 output;
 	output.bit48 = 0;
 
+//	printf("pc2 before:%llu\n", (unsigned long long)input.bit56);
 	for(int i = 0; i < 48; i++) {
 
 		temp.bit56 = input.bit56 & (0x80000000000000 >> (pc2_table[i] - 1)); 
@@ -124,6 +218,7 @@ bit_48 permutation_c_2(bit_56 input) {
 
 	}
 	output.bit48 = (out_temp.bit56 >> 8);
+//	printf("pc2 after:%llu\n", (unsigned long long)output.bit48);
 	return output;
 }
 void keygen_round(splited_56_28 input){
@@ -131,7 +226,9 @@ void keygen_round(splited_56_28 input){
 	char round_table[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1}; 
 
 	bit_56 left_shift_res;
+	left_shift_res.bit56 = 0;
 	bit_48 pc_2_res;
+	pc_2_res.bit48 = 0;
 
 	splited_56_28 left_shift_temp;
 	left_shift_temp.left = 0;
@@ -144,28 +241,33 @@ void keygen_round(splited_56_28 input){
 		left_shift_n(&left_shift_temp, round_table[i]);
 
 		left_shift_res.bit56 = 0;
-		left_shift_res = combine_left_with_right_28(left_shift_temp);
+		left_shift_res = combine_left_with_right_28(left_shift_temp);//chk this module
 
 
 		pc_2_res.bit48 = 0;
 		pc_2_res = permutation_c_2(left_shift_res);
 
 		subkey[i].bit48 = pc_2_res.bit48;
+//		printf("%llu\n", (unsigned long long)subkey[i].bit48);
 	}
 }
-void subkey_generate(bit_64 input) {
+void subkey_generate(bit_56 input) {
+	bit_64 parity_res = 0;
 
+//	parity_res = parity_chk_64(input);
+parity_res = (bit_64)0x133457799BBCDFF1;
 	splited_56_28 left_shift_tmp;
 	left_shift_tmp.left = 0;
 	left_shift_tmp.right = 0;
 
-	left_shift_tmp = permutation_c_1(input);
-
+	left_shift_tmp = permutation_c_1(parity_res);
+//	printf("pc1 res left:%x right:%x\n",left_shift_tmp.left, left_shift_tmp.right);//ok!
 	keygen_round(left_shift_tmp);
 }
 
-bit_48 expantion_permutation(splited_64_32 input) {
+bit_48 expantion_permutation(splited_64_32 input) { //chk needed
 
+//	printf("ep input:%x\n",(unsigned int)input.right);
 	char ep_table[48] = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1};
 	bit_48 output;
 	output.bit48 = 0;
@@ -174,13 +276,13 @@ bit_48 expantion_permutation(splited_64_32 input) {
 
 	for(int i = 0; i < 48; i++) {
 
-		temp.bit48 = input.right & (0x80000000 >> (ep_table[i] - 1));
+		temp.bit48 = input.right & ((unsigned int)0x80000000 >> (ep_table[i] - 1));
 		temp.bit48 <<= 16;
 		temp.bit48 = temp.bit48 << (ep_table[i] - 1);
 		temp.bit48 = temp.bit48 >> i;
-
 		output.bit48 |= temp.bit48;
 	}
+//	printf("ep output %llx\n", (unsigned long long int)output.bit48);
 	return output;
 }
 
@@ -234,7 +336,7 @@ bit_32 substitution(bit_48 input) {
 
 		tmp = (input.bit48 & (0xFC0000000000 >> (6*i))) >> (48 - (6 * (i + 1)));
 
-		printf("%d: %x\n", i+1, tmp);
+		//printf("%d: %x\n", i+1, tmp);
 		sub_val[i] = tmp;
 	}
 	char row = 0;
@@ -247,13 +349,13 @@ bit_32 substitution(bit_48 input) {
 	row = ((sub_val[i] >> 4) & 2) | (sub_val[i]  & 1);
 	col = (sub_val[i] & 0x1E) >> 1;
 	sub_val[i] = sbox[i][row][col];
-	printf("%x\n", sub_val[i]);
+//	printf("%x\n", sub_val[i]);
 	}
 
 	for(int i = 0; i < 8; i++) {
 		output |= ((bit_32)sub_val[i] & 0xF) << (28 - (i * 4));
 	}
-	printf("%x\n", output);
+//	printf("%x\n", output);
 	return output; 
 }
 
@@ -282,7 +384,7 @@ bit_32 permutation_4(bit_32 input) {
 	return output;
 }
 
-splited_64_32 festal(splited_64_32 input, bit_48 key) {//16회 반복
+splited_64_32 festal(splited_64_32 input, bit_48 key) {
 
 	bit_48 res_ep;
 	res_ep.bit48 = 0;
@@ -310,19 +412,22 @@ splited_64_32 festal(splited_64_32 input, bit_48 key) {//16회 반복
 
 splited_64_32 initial_permutation(bit_64 input) {
 
-	char ip_table[64] = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
+//	printf("ip before: %016llx\n", input);
+	uint8_t ip_table[64] = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
 	
 	bit_64 out_tmp = 0;
 	bit_64 temp = 0;
 
 	for(int i = 0; i < 64; i++) {
 
-		temp = input & (0x8000000000000000 >> (ip_table[i] - 1)); 
+		temp = input & ((bit_64)0x8000000000000000 >> (ip_table[i] - 1)); 
 		temp = temp << (ip_table[i] - 1);
 		temp = temp >> i;
 
 		out_tmp |= temp;
 	}   
+
+//	printf("ip after: %016llx\n", out_tmp);
 
 	splited_64_32 output;
 	output.left = 0;
@@ -378,16 +483,46 @@ bit_64 des_encrypt(bit_64 input) {
 	sw_res = switch_64(festal_res);
 
 	bit_64 output = initial_permutation_inverse(sw_res);
+	printf("en: %llx\n", (unsigned long long int)output);
+	return output;
+}
+
+bit_64 des_decrypt(bit_64 input) {
+
+	splited_64_32 ip_res;
+	ip_res.left = 0;
+	ip_res.right = 0;
+
+	ip_res = initial_permutation(input);
+
+	splited_64_32 festal_res;
+	festal_res = ip_res;
+
+	for(int i = 15; i >= 0; i--) {
+		festal_res = festal(festal_res, subkey[i]);
+	}
+
+	bit_64 sw_res;
+	sw_res = switch_64(festal_res);
+
+	bit_64 output = initial_permutation_inverse(sw_res);
+//	int_to_str_64(&output);
+	printf("dec: %llx\n", (unsigned long long int)output);
 	return output;
 }
 
 void main(void) {
-//	bit_64 temp = 0x4E36DD8D568284C9;
-//
-	bit_64 plain_t = (bit_64)"SOHEEMON";
-//	subkey_generate(temp);
-	des_encrypt(plain_t);
-//	bit_48 temp;
-//	temp.bit48 = 0xFFFFFFFFFFFF;
-//	substitution(temp);
+	bit_56 temp;
+	temp.bit56 = 0x4E6F7465706164;
+	subkey_generate(temp);
+	bit_64 tmp = 0;
+	unsigned char text[8] = "SOHEEMON";
+	//tmp = str_to_int_64(text);
+	//tmp = 0x0123456789abcdef;
+
+	tmp = 0xfedcba9876543210;
+	printf("plain: %llx\n", (unsigned long long int) tmp);
+	//int_to_str_64(&tmp);
+	bit_64 tmp2 = des_encrypt(tmp);
+	des_decrypt(tmp2);
 }
